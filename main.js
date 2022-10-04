b109a18c918feb480c5fe51b3c46c6759a5afa46
Eslam Osama <eo13012003@gmail.com>
2022-10-04 17:36:55 +0200

    Added counter function in JS

---
+++ main.js
@@ -0,0 +1,16 @@
+let eventDate = new Date('December-31-2023 23:59:59');
+
+let countDown = setInterval(function () {
+  let currentDate = new Date().getTime();
+  let dateDiff = eventDate - currentDate;
+  //Set Time Units
+  let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
+
+  document.querySelector('.counter .days').innerHTML = days;
+  document.querySelector('.counter .hours').innerHTML = hours;
+  document.querySelector('.counter .minutes').innerHTML = minutes;
+  document.querySelector('.counter .seconds').innerHTML = seconds;
+}, 1000);
